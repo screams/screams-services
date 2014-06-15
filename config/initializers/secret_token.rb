8ddcd7d642c9d296f87144fa8fd0e00360e5e019
Sri Vishnu Totakura <t.srivishnu@gmail.com>
2014-06-15 16:11:38 +0530

    rails init and database config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Screams::Application.config.secret_key_base = 'b146ef553a39261490412fd23b010ea0f956fcfbf83299185412160c654f774e37af00dade7213fc8740ef243fbfe2c10861dd901f1d129a2fbab734371d36cc'
